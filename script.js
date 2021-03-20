0f8431b5ccfa823fd30d217d5dde5033b3200fee
leroy <leeroynicolas62@gmail.com>
2021-03-20 23:15:57 +0100

    add array for answer

--- script.js
+++ script.js
@@ -2,31 +2,48 @@ const button = document.getElementById("talk");
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-let perroquet = document.getElementById("perroquet");
+let card = document.getElementById("card");
 let loading = document.getElementById("loading");
 loading.style.display = "none";
+card.style.display = "none";
+let addText = document.getElementById("addText");
 let talk = new SpeechSynthesisUtterance();
+
+let howareyou = ["quéche chat peu eute foute", "quo que ché que che go"];
+
+function start() {
+  recognition.start();
+}
 
 recognition.onstart = function () {
   loading.style.display = "block";
 };
+button.addEventListener("click", () => {
+  start();
+});
+
 recognition.onaudioend = function () {
   loading.style.display = "none";
 };
 
 recognition.onresult = function (event) {
-  perroquet.innerHTML += event.results[0][0].transcript + "</br>";
-  let userSentence = event.results[0][0].transcript;
-  parlesMoi(userSentence);
+  card.style.display = "block";
+  addText.innerHTML += event.results[0][0].transcript + "</br>";
+  let userSentence = event.results[0][0].transcript.toLowerCase();
+  chargesParams();
+  let sentencesArray = userSentence.split(" ");
+  console.log(sentencesArray);
+  if (sentencesArray.includes("salut")) {
+    parlesMoi("salut ça va ?");
+  } else {
+    parlesMoi("j'ai rein compris à sque tu m béfes");
+  }
 };
 
-button.addEventListener("click", () => {
-  recognition.start();
-});
 // voices qu'on veut = 4
-function chargesParams(voicesID) {
+function chargesParams() {
   const voices = window.speechSynthesis.getVoices();
-  talk.voice = voices[voicesId];
+  talk.voice = voices[4];
   talk.volume = 1;
   talk.rate = 1.2;
   talk.pitch = 1;
@@ -35,11 +52,17 @@ function chargesParams(voicesID) {
 function parlesMoi(message) {
   talk.text = message;
   window.speechSynthesis.speak(talk);
+  chargesParams(4);
+  const voices = window.speechSynthesis.getVoices();
+  talk.voice = voices[4];
+  talk.volume = 1;
+  talk.rate = 1.2;
+  talk.pitch = 1;
 }
 
-// test
+/* test
 let check = document.getElementById("test");
 check.addEventListener("click", () => {
   parlesMoi("bienvenue");
   chargesParams(4);
-});
+});*/
